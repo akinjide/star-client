7122501dabe6b8c1ef5e49b2c85b726bbac2cad8
TheHindd <thehind70@gmail.com>
2024-10-08 18:08:52 +0300

    vuetify / main page

---
+++ grad-proj-app/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1> this is Login page</h1>
+</template>
+
+<!-- <script>
+import { defineComponent } from 'vue'
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld
+  }
+})
+</script> -->
---
+++ grad-proj-app/src/views/StudentView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is student dashboard page</h1>
+  </template>
