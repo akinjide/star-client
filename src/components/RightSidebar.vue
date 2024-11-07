a6f40e010a3a5e88fd4d881f9993e1c4bfba638b
Akinjide Bankole <r@akinjide.me>
2024-11-07 19:26:14 +0200

    Feat/std dashboard (#4)
    * stu
    
    * feat: new pages
    
    * refactor: sidebar and projects page
    
    ---------
    
    Co-authored-by: TheHindd <thehind70@gmail.com>

--- src/components/RightSidebar.vue
+++ src/components/RightSidebar.vue
@@ -1,57 +1,47 @@
 <template>
-  <v-navigation-drawer absolute app permanent right location="right" class="right-sidebar">
-  <!-- <v-navigation-drawer app permanent location="right" class="right-sidebar py-1 px-5"> -->
-    <div>
-      <!-- Profile Section -->
-      <div class="profile-section">
-        <v-icon large>mdi-bell-outline</v-icon>
-        <v-avatar size="30" color="primary">
-          <v-icon>mdi-account</v-icon>
-        </v-avatar>
-        <span class="user-name">Hind Abdalla</span>
-        <v-icon>mdi-chevron-down</v-icon>
-      </div>
+  <v-navigation-drawer permanent location="right" width="300" class="py-1">
+    <v-row align="center">
+      <v-col cols="9">
+        <v-list-item
+          lines="two"
+          prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
+          subtitle="Logged in"
+          nav
+          :title="user.full_name"
+        >
+        </v-list-item>
+      </v-col>
 
-      <!-- Calendar Section -->
-      <v-card class="calendar-card pa-1 ma-1" outlined >
-        <v-date-picker v-model="selectedDate" size="small"></v-date-picker>
-      </v-card>
+      <v-col cols="3">
+        <v-btn icon="mdi-bell-outline" size="x-small"></v-btn>
+      </v-col>
+    </v-row>
 
-      <v-container class="right-sidebar">
-      </v-container>
+    <v-divider></v-divider>
 
-      <!-- Pending Tasks Section -->
-      <v-card class="tasks-card pa-1 ma-1" outlined>
-        <v-card-title>Pending Tasks</v-card-title>
+    <v-row>
+      <v-col>
+        <v-card class="calendar-card ma-4" outlined>
+          <VCalendar borderless title-position="left" :attributes="attributes" />
+        </v-card>
+      </v-col>
+    </v-row>
 
-        <v-list>
-          <div>
-            <v-list-subheader class="text-uppercase dashboard-title">Upcoming Tasks</v-list-subheader>
+    <v-row>
+      <v-col>
+        <v-card class="tasks-card ma-4" outlined>
+          <v-card-title class="dashboard-title">Pending Tasks</v-card-title>
 
-            <v-list-item v-for="(task, index) in tasks" :key="index">
+          <v-list>
+            <v-list-item v-for="(task, index) in tasks" :key="index" class="pb-4">
               <v-list-item-title>{{ task.task_name }}</v-list-item-title>
+
               <v-list-item-subtitle>{{ endsAt(task.task_ends_at) }}</v-list-item-subtitle>
             </v-list-item>
-          </div>
-        </v-list>
-
-        <v-list>
-          <v-list-item v-for="(task, index) in t" :key="index">
-            <v-list-item-avatar>
-              <v-icon color="red">mdi-circle</v-icon>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title class="task-title">{{ task.title }}</v-list-item-title>
-              <v-list-item-subtitle>{{ task.type }} • {{ task.time }}</v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-icon small>mdi-dots-horizontal</v-icon>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
-
-      </v-card>
-    </div>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-navigation-drawer>
 </template>
 
@@ -60,24 +50,46 @@ import { useDateFormat } from '@vueuse/core'
 
 export default {
   name: 'RightSidebar',
+  components: {},
   props: {
     tasks: {
       type: Array,
+      required: true
+    },
+    user: {
+      type: Object,
       required: true
     }
   },
   data () {
     return {
-      selectedDate: new Date(),
-      t: [
-        { title: 'Speak with stakeholders', type: 'Design', time: '10:00 am' },
-        { title: 'Input Rubrics Data', type: 'Design', time: '10:00 am' }
+      attributes: [
+        {
+          highlight: {
+            color: 'blue',
+            fillMode: 'light'
+          },
+          popover: {
+            visibility: 'click',
+            label: 'Task 1: Deadline'
+          },
+          dates: [new Date()]
+        },
+        {
+          highlight: 'green',
+          dot: true,
+          dates: [[new Date(2024, 10, 8), new Date(2024, 10, 12)]],
+          popover: {
+            visibility: 'click',
+            label: 'Task 2: Deadline'
+          }
+        }
       ]
     }
   },
   methods: {
     endsAt (t) {
-      return useDateFormat(t, 'YYYY-MM-DD HH:mm:ss')
+      return useDateFormat(t, 'dddd, YYYY-MM-DD HH:mm a')
     }
   }
 }
@@ -89,22 +101,13 @@ export default {
   justify-content: center;
   align-content: space-evenly;
 }
-.profile-section {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 20px;
-}
 
-.user-name {
-  margin: 0 8px;
-  font-weight: bold;
-  color: #445a99;
+.calendar-card :deep(.vc-weekday-1, .vc-weekday-7) {
+  color: #6366f1;
 }
 
 .calendar-card, .tasks-card {
   margin-top: 20px;
-  padding: 16px;
   background-color: #ffffff;
 }
 
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,8 +1,43 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col cols="12">
-        <h1>Project View</h1>
+      <v-col cols="10">
+        <v-table
+          height="100vh"
+          fixed-header
+        >
+          <thead>
+            <tr>
+              <th class="text-left text-uppercase">
+                Project Title
+              </th>
+              <th class="text-left text-uppercase">
+                Supervisor
+              </th>
+              <th class="text-left text-uppercase">
+                Availability
+              </th>
+              <th class="text-left text-uppercase">
+                Group
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="project in projects"
+              :key="project.project_title"
+            >
+              <td>{{ project.project_title }}</td>
+              <td>{{ project.supervisor_name }}</td>
+              <td>
+                <v-chip class="ma-2 text-capitalize" :color="project.status == 'available' ? 'green' : 'red'" prepend-icon="mdi-circle">
+                  {{ project.status }}
+                </v-chip>
+              </td>
+              <td>{{ project.group_name }}</td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-col>
     </v-row>
   </v-container>
@@ -15,7 +50,28 @@ import { useUserStore } from '@/stores/user'
 export default {
   name: 'Project',
   data () {
-    return {}
+    return {
+      projects: [
+        {
+          project_title: 'Online Management Solution for Graduation Projects',
+          supervisor_name: 'Assoc. Prof. Dr. Cem Ergun',
+          status: 'unavailable',
+          group_name: 'Group 4'
+        },
+        {
+          project_title: 'Video Streaming Platform',
+          supervisor_name: 'Assoc. Prof. Dr. Guruc Oz',
+          status: 'available',
+          group_name: '-'
+        },
+        {
+          project_title: 'Image Processing',
+          supervisor_name: 'Prof. Dr. Mohammed Salamah',
+          status: 'available',
+          group_name: '-'
+        }
+      ]
+    }
   },
   components: {},
   methods: {
